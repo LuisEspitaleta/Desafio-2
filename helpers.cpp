d16675e7fda84307acb3423e41c6ac18120d9674
Jojhan-Restrepo <161488744+Jojhan-Restrepo@users.noreply.github.com>
2024-05-08 16:50:43 -0500

    suma

---
+++ helpers.cpp
@@ -0,0 +1,10 @@
+int sumarSegundos() {
+    int suma = 0;
+    for (int i = 0; i < numArreglos; ++i) {
+        // Verificar que haya al menos dos elementos en el subarreglo
+        if (arregloDelineametro[i][1] != -1) {
+            suma += arregloDelineametro[i][1]; // Sumar el segundo elemento del subarreglo
+        }
+    }
+    return suma;
+}
